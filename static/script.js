bcdcf9457e2b8cd14bb57a5c8e320610ed272a4b
baylagas <balbino_a@hotmail.com>
2021-02-05 20:46:15 -0600

    added new html elements and javascript functions

--- static/script.js
+++ static/script.js
@@ -1,5 +1,55 @@
-var txt = document.getElementById("title").innerHTML;
+function checkInputText(){
+    var txt = document.getElementById("user").value;
+    document.getElementById("span1").innerHTML = txt;
+}
 
-document.getElementById("main").innerHTML="hola a todos";
+function checkInputPassw(){
+    var txt = document.getElementById("passw").value;
+    document.getElementById("span2").innerHTML = txt;
+}
 
-document.getElementById("title").innerHTML = txt + "::phoenix" + "||"+document.getElementById("user").value;
+function checkInputRadio(){
+    var foodArray = document.getElementsByName("food");
+    for(var i=0, length=foodArray.length; i<length; i++){
+        if(foodArray[i].checked){
+            document.getElementById("span3").innerHTML = foodArray[i].value;
+            break;
+        }
+    }
+}
+
+function checkInputCheckBox(){
+    var vehicleArray = document.getElementsByName("vehicle");
+    var txt = "";
+    for(var i=0, length=vehicleArray.length; i<length; i++){
+        if(vehicleArray[i].checked){
+            txt += vehicleArray[i].value + "|";
+        }
+    }
+    txt = txt.substring(0,(txt.length-1));
+    document.getElementById("span4").innerHTML = txt;
+
+    var resultArray = txt.split("|");
+    document.getElementById("span4").innerHTML = txt + ":" + resultArray[0];
+
+}
+
+function checkInputColor(){
+    var colorHex = document.getElementById("color").value;
+    document.getElementById("span5").innerHTML = colorHex;
+    document.getElementById("title").style.color = colorHex;
+}
+
+function checkInputTime(){
+    var time = document.getElementById("time").value;
+    document.getElementById("span6").innerHTML = time;
+}
+
+function checkInputDateTimeLocal(){
+    var time = document.getElementById("birthdaytime").value;
+    document.getElementById("span7").innerHTML = time;
+    var resArray = time.split("T");
+    alert(resArray[0]);
+    resArray = resArray[0].split("-");
+    alert(resArray[2]);
+}
