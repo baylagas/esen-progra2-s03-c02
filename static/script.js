ad2405bb64c1304bf2699eaea56be8e69e43a982
baylagas <balbino_a@hotmail.com>
2021-02-05 18:53:58 -0600

    init files

---
+++ static/script.js
@@ -0,0 +1,5 @@
+var txt = document.getElementById("title").innerHTML;
+
+document.getElementById("main").innerHTML="hola a todos";
+
+document.getElementById("title").innerHTML = txt + "::phoenix" + "||"+document.getElementById("user").value;
